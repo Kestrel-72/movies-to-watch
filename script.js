ea4b023dc1947cea4d6c15590660a0cde941725b
Roman <kavoriya@gmail.com>
2023-03-08 16:10:07 +0500

    Build: create and delete form to add a new movie
    - Clicking on a "new" button creates a form at "aside"
    section. Clicking on "clear" button removes the form.
    - "appendChild" methods have been replaced with "append"
    to add text to buttons without creating dedicated span for text*
    
    * For some reason I couldn't add text AFTER an icon span with
    .appendChild and .textContent. So I used .append to add them both
    simultaneously.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let moviesArray = [];
+let aside = document.querySelector(".sidebar");
+let newMovieButton = document.querySelector(".add-new-movie");
+newMovieButton.addEventListener("click", createFormSection);
 
 let toWatchList = document.querySelector(".to-watch-list");
 
@@ -8,16 +11,6 @@ function Movie(title, releaseDate, isWatched, rating) {
   this.isWatched = isWatched;
   this.rating = rating;
 }
-
-let saveChangesButton = document.querySelector(".save-movie");
-let discardChangesButton = document.querySelector(".discard-changes");
-
-saveChangesButton.addEventListener("click", () => {
-  if (document.getElementById("title").value != "") {
-    saveMovie(); 
-    createListItem(moviesArray[moviesArray.length-1]);
-  }
-});
 
 function saveMovie() {
   let title = document.getElementById("title").value;
@@ -34,28 +27,106 @@ function createListItem(movie) {
 
   let newH3 = document.createElement("h3");
   newH3.textContent = movie.title;
-  newListItem.appendChild(newH3);
+  newListItem.append(newH3);
 
   let newReleaseDate = document.createElement("span");
   newReleaseDate.classList.add("year");
   if (movie.releaseDate != "") {
     newReleaseDate.textContent = ` (${movie.releaseDate})`;
   }
-  newH3.appendChild(newReleaseDate);
+  newH3.append(newReleaseDate);
 
   let ratingDiv = document.createElement("div");
   ratingDiv.classList.add("rating");
   ratingDiv.textContent = movie.rating;
-  newListItem.appendChild(ratingDiv);
+  newListItem.append(ratingDiv);
 
   let starSpan = document.createElement("span");
   starSpan.classList.add("material-icons");
   if (movie.rating != "") {
     starSpan.textContent = "star";
   }
-  ratingDiv.appendChild(starSpan);
-  
-  toWatchList.appendChild(newListItem);
+  ratingDiv.append(starSpan);
+  toWatchList.append(newListItem);
 }
 
+function createFormSection() {
+  clearAside();
+  let h2 = document.createElement("h2");
+  h2.textContent = "New movie";
+
+  let form = document.createElement("form");
+  form.classList.add("movie-form");
+
+  let ul = document.createElement("ul");
+
+  for (let i = 0; i < 4; i++) {
+    let li = document.createElement("li");
+    let label = document.createElement("label");
+    let input = document.createElement("input");
+    if (i == 0) {
+      label.setAttribute("for", "title");
+      label.textContent = "Title: ";
+      input.setAttribute("id", "title");
+      input.setAttribute("type", "text");
+    } else if (i == 1) {
+      label.setAttribute("for", "year");
+      label.textContent = "Release date: ";
+      input.setAttribute("id", "year");
+      input.setAttribute("type", "number");
+    } else if (i == 2) {
+      label.setAttribute("for", "is-watched");
+      label.textContent = "I watched it ";
+      input.setAttribute("id", "is-watched");
+      input.setAttribute("type", "checkbox");
+    } else if (i == 3) {
+      label.setAttribute("for", "stars");
+      label.textContent = "Rating";
+      input.setAttribute("id", "stars");
+      input.setAttribute("type", "number");
+    }
+    li.append(label, input);
+    ul.append(li);
+  }
+  form.append(ul);
+  aside.append(h2);
+ 
+
+  let formButtonsDiv = document.createElement("div");
+  formButtonsDiv.classList.add("form-buttons"); 
+
+  let discardChangesButton = document.createElement("button");
+  discardChangesButton.classList.add("discard-changes");
+  discardChangesButton.setAttribute("type", "button");
+  let spanOne = document.createElement("span");
+  spanOne.classList.add("material-icons");
+  spanOne.textContent = "delete";
+  discardChangesButton.append(spanOne, " Cancel");
+
+  discardChangesButton.addEventListener("click", clearAside);
+  
+
+  let saveMovieButton = document.createElement("button");
+  saveMovieButton.classList.add("save-movie");
+  saveMovieButton.setAttribute("type", "button");
+  let spanTwo = document.createElement("span");
+  spanTwo.classList.add("material-icons");
+  spanTwo.textContent = "bookmark_add";
+  saveMovieButton.append(spanTwo, "Save");
+
+  saveMovieButton.addEventListener("click", () => {
+    if (document.getElementById("title").value != "") {
+      saveMovie(); 
+      createListItem(moviesArray[moviesArray.length-1]);
+    }
+  });
+  
+  formButtonsDiv.append(discardChangesButton, saveMovieButton);
+  form.append(formButtonsDiv);
+  aside.append(form);
+}
+
+function clearAside() {
+  aside.innerHTML = "";
+}
 
